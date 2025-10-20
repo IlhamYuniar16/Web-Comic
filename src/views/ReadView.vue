308d316dfa831b635c154495e6a7675e32b10a58
Nama Anda <email@anda.com>
2025-10-20 09:48:47 +0700

    last chache commit

--- src/views/ReadView.vue
+++ src/views/ReadView.vue
@@ -1,5 +1,5 @@
 <template>
-        <header class="shadow-md bg-white dark:bg-gray-900 dark:text-white transition-all duration-300">
+        <header class="shadow-md hidden md:block bg-white dark:bg-gray-900 dark:text-white transition-all duration-300">
         <main class="flex justify-between items-center p-4 max-w-6xl mx-auto">
             <main class="md:flex gap-5 hidden items-center justify-center py-3  text-sm font-medium text-gray-700">
               <a href="/" class="rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 cursor-pointer">Beranda</a>
@@ -10,16 +10,8 @@
                 <img class="w-7" src="../assets/img/korea.webp" alt=""> Manhwa Populer
               </a>
             </main>
-            <main class="flex md:hidden gap-2 items-center justify-center py-3 text-sm font-medium text-gray-700">
-              <a href="/" class="rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 cursor-pointer">Beranda</a>
-              <a href="/mangapopuler" class="rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 cursor-pointer flex gap-2 items-center">
-                <img class="w-7" src="../assets/img/jepang.webp" alt="">Manga 
-              </a>
-              <a href="/manhwapopuler" class="rounded-md px-4 py-2 bg-gray-200 hover:bg-gray-300 cursor-pointer flex gap-2 items-center">
-                <img class="w-7" src="../assets/img/korea.webp" alt="">Manhwa 
-              </a>
-            </main>
-          <div class="flex gap-6 items-center">
+
+          <div class="md:flex hidden gap-6 items-center justify-end">
      
             <button class="flex bg-gray-200 hover:bg-gray-300 p-2 rounded-full" @click="darkMode">
               <svg v-if="openDarkMode" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -43,9 +35,8 @@
             </button>
           </div>
         </main>
-    
       </header>
-      <section class="dark:bg-gray-900 bg-gray-200 dark:text-white transition-all duration-300">
+      <section class="bg-white md:dark:bg-gray-900 md:dark:text-white transition-all duration-300">
           <div class="max-w-5xl mx-auto md:px-4 py-6 font-sans ">
             <div v-if="loading" class="text-center flex justify-center py-10 text-gray-500">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 animate-spin"><path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" /></svg>
@@ -56,9 +47,9 @@
             </div>
         
             <div v-else> 
-              <h1 class="text-3xl font-bold text-center mb-2 md:px-0 px-10 dark:text-white transition-all duration-300" >{{ title }}</h1>
+              <h1 class="text-3xl font-bold text-center mb-2 md:px-0 px-10 transition-all duration-300" >{{ title }}</h1>
               <p class="text-center text-gray-600 mb-6">
-                Kreator: <span class="font-medium text-black  dark:text-white transition-all duration-300">{{ comic.creator }}</span>
+                Kreator: <span class="font-medium text-black md:dark:text-white transition-all duration-300">{{ comic.creator }}</span>
               </p>
         
               <div class="flex flex-col items-center">
@@ -72,19 +63,20 @@
                 />
               </div>
         
-              <div class="flex justify-between mt-10">
+              <div class="flex justify-between items-center md:px-27 mt-5 px-5">
                 <button
                   @click="prevChapter"
                   :disabled="currentChapter <= 1"
-                  class="px-5 py-2 bg-gray-300 rounded-md hover:bg-gray-400 disabled:opacity-50 font-medium"
+                  class="px-5 py-2 bg-gray-500 md:dark:bg-gray-300 rounded-md hover:bg-gray-400 disabled:opacity-50 font-medium"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 dark:text-black text-white"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" /></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 md:dark:text-black text-white"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" /></svg>
                 </button>
+                <a href="/" class="bg-gray-500 md:px-0 px-5 md:hidden block py-2 rounded-md md:dark:text-black text-white md:dark:bg-gray-300">Beranda</a>
                 <button
                   @click="nextChapter"
-                  class="px-5 py-2 bg-gray-300 rounded-md hover:bg-gray-400 font-medium"
+                  class="px-5 py-2 md:dark:bg-gray-300 bg-gray-500 rounded-md hover:bg-gray-400 font-medium"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 dark:text-black text-white"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" /></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 md:dark:text-black text-white"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" /></svg>
                 </button>
               </div>
             </div>
